6048cc7794670986516bf21723ee5db9c27ee9b3
Satoshi N. M <snmatsutake@yahoo.co.jp>
2018-01-27 18:38:56 -0800

    Try moving GPIO receive queue into ShiftTask but it crashes:
    Guru Meditation Error: Core  0 panic'ed (StoreProhibited)
    . Exception was unhandled.
    Register dump:
    PC      : 0x40086138  PS      : 0x00060133  A0      : 0x8008582f  A1      : 0x3ffc98c0
    0x40086138: uxPortCompareSet at esp32/esp-idf/components/freertos/./tasks.c:3529
     (inlined by) vPortCPUAcquireMutexIntsDisabled at esp32/esp-idf/components/freertos/./portmux_impl.h:86
     (inlined by) vTaskEnterCritical at esp32/esp-idf/components/freertos/./tasks.c:4211
    
    A2      : 0x00a5a5ed  A3      : 0x00000000  A4      : 0x00000000  A5      : 0x00000000
    A6      : 0x00000000  A7      : 0x00000000  A8      : 0x0000cdcd  A9      : 0x0000cdcd
    A10     : 0xb33fffff  A11     : 0x0000abab  A12     : 0x00060120  A13     : 0x00000001
    A14     : 0x00060120  A15     : 0x00060a23  SAR     : 0x0000000a  EXCCAUSE: 0x0000001d
    EXCVADDR: 0x00a5a5ed  LBEG    : 0x4000c2e0  LEND    : 0x4000c2f6  LCOUNT  : 0xffffffff
    
    Backtrace: 0x40086138:0x3ffc98c0 0x4008582c:0x3ffc98e0 0x4010a6d5:0x3ffc9920
    0x40086138: uxPortCompareSet at esp32/esp-idf/components/freertos/./tasks.c:3529
     (inlined by) vPortCPUAcquireMutexIntsDisabled at esp32/esp-idf/components/freertos/./portmux_impl.h:86
     (inlined by) vTaskEnterCritical at esp32/esp-idf/components/freertos/./tasks.c:4211
    
    0x4008582c: xQueueGenericReceive at esp32/esp-idf/components/freertos/./queue.c:2037
    
    0x4010a6d5: ShiftTask at esp32/ssd1306-esp32-idf-testing/main/./main.c:480 (discriminator 4)
    
    Rebooting...

--- main/main.c
+++ main/main.c
@@ -20,6 +20,13 @@
 #include "lwip/netdb.h"
 #include "lwip/dns.h"
 
+// Based on example https://github.com/espressif/esp-idf/blob/master/examples/peripherals/gpio/main/gpio_example_main.c
+#define GPIO_INPUT_IO_0     4
+#define GPIO_OUTPUT_IO_0     0
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static xQueueHandle gpio_evt_queue = NULL;
+
 
 /* Constants that aren't configurable in menuconfig */
 /*
@@ -516,6 +523,11 @@ void ShiftTask( void* Param ) {
             Delay= 3;
         }
 
+        uint32_t io_num;
+        if(xQueueReceive(gpio_evt_queue, &io_num, 0)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+
         vTaskDelay( pdMS_TO_TICKS( Delay ) );
     }
 }
@@ -649,21 +661,13 @@ static void http_get_task(void *pvParameters)
     xTaskCreate( ShiftTask, "ShiftTask", 4096, NULL, 3, &xTask );
 }
 
-// Based on example https://github.com/espressif/esp-idf/blob/master/examples/peripherals/gpio/main/gpio_example_main.c
-#define GPIO_OUTPUT_IO_0    0
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0))
-#define GPIO_INPUT_IO_0     4
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0))
-#define ESP_INTR_FLAG_DEFAULT 0
-
-static xQueueHandle gpio_evt_queue = NULL;
-
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
+/*
 static void gpio_task_example(void* arg)
 {
     uint32_t io_num;
@@ -673,56 +677,38 @@ static void gpio_task_example(void* arg)
         }
     }
 }
+*/
 
 void setup_gpio(void) {
+    // Edge-triggered GPIO input
     gpio_config_t io_conf;
-    //disable interrupt
-    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
 
-    //interrupt of rising edge
-    io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    //set as input mode    
+    io_conf.intr_type = GPIO_PIN_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = ((1ULL<<GPIO_INPUT_IO_0));
     io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
+    io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
-
-    //change gpio intrrupt type for one pin
-    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
 
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     //start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    //xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
     //install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
 
-    //remove isr handler for gpio number.
-    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin again
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
 
-    int cnt = 0;
-    while(1) {
-        printf("cnt: %d\n", cnt++);
-        //vTaskDelay(1000 / portTICK_RATE_MS);
-        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
-    }
+    // GPIO output
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.pin_bit_mask = ((1ULL<<GPIO_OUTPUT_IO_0));
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+    gpio_set_level(GPIO_OUTPUT_IO_0, 0);
 }
 
 void app_main( void ) {
